8b1cf3fc39742f36207b365e84878f65f09c05c5
김영민 <38073220+youngmeen@users.noreply.github.com>
2019-11-06 17:23:21 +0900

    업로드까지 완료

--- app/src/main/java/com/example/snsver1/MainActivity.kt
+++ app/src/main/java/com/example/snsver1/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.snsver1
 
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.snsver1.navigation.*
 
-import com.example.snsver1.navigation.AlarmFragment
-import com.example.snsver1.navigation.DetailViewFragment
-import com.example.snsver1.navigation.GridFragment
-import com.example.snsver1.navigation.UserFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -28,6 +30,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo ->{
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this,AddPhotoActivity::class.java))
+                }
                 return true
             }
             R.id.action_favorite_alarm ->{
@@ -48,5 +53,6 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
 }
